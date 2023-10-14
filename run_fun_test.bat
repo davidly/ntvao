f42e573d097c1cedb0c8915cb1e95d8eb8b1d280
davidly <davidly_lee@hotmail.com>
2023-10-14 12:54:55 -0700

    fix typo in comment

--- run_fun_test.bat
+++ run_fun_test.bat
@@ -1,4 +1,4 @@
-rem on success with print a 'S' and exit the app
+rem on success will print a 'S' and exit the app
 rem on failure will infinite loop at the failure point
 
 .\ntvao /a:400 -c -h 6502_functional_test.hex
