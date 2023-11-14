5d7797bb2152351f13b8097f8d33c5d467b59e46
davidly <davidly_lee@hotmail.com>
2023-11-14 15:15:23 -0800

    work around perf bug with win arm64 cl.exe

---
+++ marm64r.bat
@@ -0,0 +1,2 @@
+cl /nologo ntvao.cxx mos6502.cxx /MT /Ot /Ox /Ob2 /Oi /Qpar /O2 /EHac /Zi /DNDEBUG /link ntdll.lib user32.lib /OPT:REF
+
--- mos6502.hxx
+++ mos6502.hxx
@@ -55,7 +55,6 @@ struct MOS_6502
     uint8_t op_rotate( uint8_t rotate, uint8_t val );
     void op_cmp( uint8_t lhs, uint8_t rhs );
     void op_php();
-    void op_rts();
 
     void set_nz( uint8_t val )
     {
