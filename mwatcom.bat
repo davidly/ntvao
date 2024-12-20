cd1695eba4070c5fbb9d10fc693dcdbec89815b4
davidly <davidly_lee@hotmail.com>
2024-12-19 20:00:59 -0800

    update watcom build file for local shared headers

--- mwatcom.bat
+++ mwatcom.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
-SET WATCOM=C:\WATCOM
+SET WATCOM=..\WATCOM
 SET PATH=%WATCOM%\BINNT64;%WATCOM%\BINNT;%PATH%
 SET EDPATH=%WATCOM%\EDDAT
 SET INCLUDE=%WATCOM%\H;%WATCOM%\H\NT;..\djl
 
-wcl -q -zp=1 -ml -obmr -oh -ei -oi -s -0 -xs -j -oe=160 -ol+ -ot ntvao.cxx mos6502.cxx -bcl=DOS -k8192 -fe=ntvaodos.exe /DWATCOM /DNDEBUG
+wcl -q -zp=1 -ml -obmr -oh -ei -oi -s -0 -xs -j -oe=160 -ol+ -ot ntvao.cxx mos6502.cxx -bcl=DOS -k8192 -fe=ntvaodos.exe -DWATCOM -DNDEBUG -lr -I.
 
